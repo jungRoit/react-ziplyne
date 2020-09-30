079bc01d48a426ddf6024d66cc053549cb555a24
jungRoit <roit.github@gmail.com>
2020-09-30 09:47:40 +0545

    title added

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -9,16 +9,16 @@ class Home extends React.Component {
     return (
       <div className="container">
         <div className="cover-section">
-          <div className="cover-header">
-              header
-          </div>
+          <div className="cover-header">header</div>
           <div className="cover-content">
-              content
+            <div className="title-box">
+              <h1 className="cover-cotent-title">Lorem Ipsum Dolar Lorem</h1>
+              <h1 className="cover-cotent-title">Ipsum Dolar Amet.</h1>
+            </div>
           </div>
         </div>
         <div className="content-section">
-        <h1>Rest</h1>
-
+          <h1>Rest</h1>
         </div>
       </div>
     );
