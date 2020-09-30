17f29396ddc1898acbbbae79c757ce8012877bae
jungRoit <roit.github@gmail.com>
2020-09-30 19:01:44 +0545

    Updates

---
+++ precache-manifest.5ee0caeb4d73bcd9bf8b9d89a8dfb45c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4018408fd57b8ab70f51ef0e02abda10",
+    "url": "/react-ziplyne/index.html"
+  },
+  {
+    "revision": "cc5799f7ee784aa92560",
+    "url": "/react-ziplyne/static/css/main.b723b55e.chunk.css"
+  },
+  {
+    "revision": "877bc02536590a0a8cde",
+    "url": "/react-ziplyne/static/js/2.6034a8dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ziplyne/static/js/2.6034a8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5799f7ee784aa92560",
+    "url": "/react-ziplyne/static/js/main.5048f4ef.chunk.js"
+  },
+  {
+    "revision": "d1976c46fb3c702842bf",
+    "url": "/react-ziplyne/static/js/runtime-main.c227be82.js"
+  },
+  {
+    "revision": "9502599b4d2af3fd480603e00d7a7d04",
+    "url": "/react-ziplyne/static/media/Diamond.9502599b.png"
+  },
+  {
+    "revision": "d4d30fe82aeea8237c6784576d60e62a",
+    "url": "/react-ziplyne/static/media/cover.d4d30fe8.png"
+  },
+  {
+    "revision": "9dbb9be6e73932de776fa951623dd657",
+    "url": "/react-ziplyne/static/media/desktop.9dbb9be6.png"
+  },
+  {
+    "revision": "72e77b20435d91b8258b3fc81e2105f7",
+    "url": "/react-ziplyne/static/media/logo.72e77b20.png"
+  },
+  {
+    "revision": "4bed7c969974fb44b3ac21c2650c30c3",
+    "url": "/react-ziplyne/static/media/wave.4bed7c96.svg"
+  }
+]);
